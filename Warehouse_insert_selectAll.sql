544e3f497847b185cf453dd9050786c0d6a70630
Matija1988 <matijapavkovic74@gmail.com>
2024-05-31 20:05:46 +0200

    Warehouse insert, select all

---
+++ Warehouse_insert_selectAll.sql
@@ -0,0 +1,27 @@
+INSERT INTO
+	"Warehouse" (
+		"AddressStreet",
+		"AddressCity",
+		"AddressCountry",
+		"Name"
+	) VALUES
+	( 
+		'Eailing Whitemead House tower No 2',
+	'London',
+	'England',
+	'Trotter Main'
+	),
+	(
+	'Champs-Élysées 231',
+	'Paris',
+	'France',
+	'Apres moi le deluge'
+	),
+	(
+	'Upper New York bay 6',
+	'New York',
+	'United States of America',
+	'Lucky Lucianos imports-exports'
+	);
+
+select * from "Warehouse";
